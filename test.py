e30af32b66c76c9b8e72c2f4640d2d057c4511d6
noble-noob159a <thuanluong1808@gmail.com>
2026-04-26 22:15:47 +0700

    rm test.py

--- test.py
+++
@@ -1,5 +0,0 @@
-import torch
-
-print(torch.cuda.is_available())
-#https://github.com/Gourieff/Assets/raw/main/Insightface/insightface-0.7.3-cp311-cp311-win_amd64.whl
-# py -3.11 -m pip install -r requirements.txt
